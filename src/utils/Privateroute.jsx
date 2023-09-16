ad68d11227502b24e973ed5078017a21674264f9
iam-anuroop <anuroopku10@gmail.com>
2023-09-16 21:19:03 +0530

    update

--- src/utils/Privateroute.jsx
+++ src/utils/Privateroute.jsx
@@ -1,15 +1,15 @@
-// import { Children, useContext } from "react";
-// import { Route, Navigate } from "react-router-dom"; 
-// import AuthContext from "../context/AuthContext";
+import { Children, useContext } from "react";
+import { Route, Navigate } from "react-router-dom"; 
+import AuthContext from "../context/AuthContext";
 
-// const Privateroute = ({ Component, ...rest }) => {
-//   // Check if the user is authenticated here
-// const {user} = useContext(AuthContext) 
-//   console.log('user',user);
-//   console.log('user',Component);
-// return (
-//     <>{user? Component:<Navigate  to='login/' />}</>
-//   );
-// };
+const Privateroute = ({ Component, ...rest }) => {
+  // Check if the user is authenticated here
+const {user} = useContext(AuthContext) 
+  console.log('user',user);
+  console.log('user',Component);
+return (
+    <>{user? Component:<Navigate  to='login/' />}</>
+  );
+};
 
-// export default Privateroute;
+export default Privateroute;
